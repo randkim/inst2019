aecf19d5473de61632ce83187be63c9de58259e3
randkim <40221733+randkim@users.noreply.github.com>
2019-07-25 19:03:20 +0800

    Update glacier_debris_dynamics.py

--- glacier_debris_dynamics.py
+++ glacier_debris_dynamics.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 #Initial Conditions
 debris_temp_surface = [275]
 debris_temp_btm = [268]
-ice_temp = [265]
+ice_temp = [268]
 
 #Constants
 #Heatcap are reduced by a factor of 100 for now to make calculations faster
@@ -15,7 +15,7 @@ density_rock = 2243 #Based on Marl
 heatcap_rock = 2000/10**(2)#Average value
 
 #Value obtained from https://pubs.usgs.gov/of/1988/0441/report.pdf
-thermal_conductivity_of_rock = 2.390 * 10**(-3) * 4.184 * 100  #Conversion of cal/cm to joules/m
+thermal_conductivity_of_rock = 2.390 * 10**(-3) * 4.184 * 100 * 10  #Conversion of cal/cm to joules/m
 heat_transfer_coefficient_water = 500 #to 10,000 Wm^(-2)K
 
 soil_albedo = 0.17
@@ -31,7 +31,7 @@ bottom_temp_gradient = []
 avg_temp_gradient = []
 avg_temp = []
 
-ice_mass = [3.1*10**(-5)]
+ice_mass = [3.1*10**(-6)]
 
 #While ice is below melting temperature
 while ice_temp[-1] <= 273:
@@ -64,13 +64,14 @@ while ice_temp[-1] <= 273:
     # =============================================================================
     
     #Step 3: Ice temperature increases
-    change_ice_temp = thermal_conductivity_of_rock * (avg_temp_gradient[-1] - ice_temp[-1])/(density_ice * heatcap_ice)
-    ice_temp.append(ice_temp[-1] - change_ice_temp)
+    change_ice_temp = thermal_conductivity_of_rock * (debris_temp_btm[-1] - ice_temp[-1])/(density_ice * heatcap_ice)
+    ice_temp.append(ice_temp[-1] + change_ice_temp)
+    print(ice_temp[-1])
     
     
 #Once ice heats up to melting temperature    
 if ice_temp[-1] > 273:
-    while ice_mass[-1] > 2.8*10**(-5):
+    while abs(ice_mass[-1]) < 1*10**(-5):
         change_debris_surface_temp = (1 - soil_albedo) * solar_constant/(density_rock * heatcap_rock)
         debris_temp_surface.append(debris_temp_surface[-1] + change_debris_surface_temp)
         
@@ -91,10 +92,12 @@ if ice_temp[-1] > 273:
 # =============================================================================
         
         #Step 3: Ice mass loss calculation
-        ice_mass_loss = - thermal_conductivity_of_rock * (avg_temp_gradient[-1] -ice_temp[-1])/latent_fusion
+        ice_mass_loss = - thermal_conductivity_of_rock * (debris_temp_btm[-1] -ice_temp[-1])/latent_fusion
         ice_mass.append(ice_mass_loss)
         
         ice_temp.append(ice_temp[-1])
+        
+        print(ice_mass_loss)
 
 
 fig, axs = plt.subplots(2,2)
