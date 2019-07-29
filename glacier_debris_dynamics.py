6fc75dc65a8907d74c3f5050ac9c31dc331e7fea
KarLing97 <53247435+KarLing97@users.noreply.github.com>
2019-07-29 23:18:20 +0800

    Update glacier_debris_dynamics.py

--- glacier_debris_dynamics.py
+++ glacier_debris_dynamics.py
@@ -22,7 +22,9 @@ soil_albedo = 0.17
 solar_constant = 1400
 
 #Lists
-debris_depth = list(range(0, 10))
+debris_depth = []
+for i in range(0,10):
+    debris_depth.append(i)
 
 surface_temp_gradient = []
 bottom_temp_gradient = []
@@ -30,42 +32,50 @@ avg_temp_gradient = []
 avg_temp = []
 
 ice_mass = [3.1*10**(-5)]
-mass_loss = []
+
+#to apply day-night cycle
+time = 0
 
 #While ice is below melting temperature
 while ice_temp[-1] <= 273:
-    #Step 1: Solar radiation heats up the top surface of the debris layer
-    change_debris_surface_temp = (1 - soil_albedo) * solar_constant/(density_rock * heatcap_rock)
-    debris_temp_surface.append(debris_temp_surface[-1] + change_debris_surface_temp)
+    #day or night
+    if time %% 2 == 0:
     
-    #Step 2: The radiation also heats up the bottom of the debris, but at a factor reduced by the depth of the debris
-    debris_temp_btm.append(debris_temp_btm[-1] + change_debris_surface_temp/max(debris_depth))
+        #Step 1: Solar radiation heats up the top surface of the debris layer
+        change_debris_surface_temp = (1 - soil_albedo) * solar_constant/(density_rock * heatcap_rock)
+        debris_temp_surface.append(debris_temp_surface[-1] + change_debris_surface_temp)
     
-    #Step 3: Two thermal gradients form. One from the updated surface temperature to the bottom, and another from the botton (which is at ice temperature) to the surface. Take the average as the actual gradient.
-    for i in range(len(debris_depth)):
-        surface_temp_gradient.append(-thermal_conductivity_of_rock * debris_depth[i] + debris_temp_surface[-1])
-        bottom_temp_gradient.append(-thermal_conductivity_of_rock * debris_depth[i] + debris_temp_btm[-1])
+        #Step 2: The radiation also heats up the bottom of the debris, but at a factor reduced by the depth of the debris
+        debris_temp_btm.append(debris_temp_btm[-1] + change_debris_surface_temp/max(debris_depth))
+    
+        #Step 3: Two thermal gradients form. One from the updated surface temperature to the bottom, and another from the botton (which is at ice temperature) to the surface. Take the average as the actual gradient.
+        for i in range(len(debris_depth)):
+            surface_temp_gradient.append(-thermal_conductivity_of_rock * debris_depth[i] + debris_temp_surface[-1])
+            bottom_temp_gradient.append(-thermal_conductivity_of_rock * debris_depth[i] + debris_temp_btm[-1])
+
+        for i in range(len(surface_temp_gradient)):
+            avg_temp_gradient.append((surface_temp_gradient[i] + bottom_temp_gradient[i])/2)
+
+        avg_temp.append(np.average(avg_temp_gradient))
+
+    # =============================================================================
+    #         if avg_temp_gradient[i] - surface_temp_gradient[i] != 0:
+    #             print("Valid")
+    # =============================================================================
+
+        # =============================================================================
+        # plt.plot(surface_temp_gradient, debris_depth)
+        # plt.plot(bottom_temp_gradient, debris_depth)
+        # plt.plot(avg_temp_gradient, debris_depth)
+        # =============================================================================
+
+        #Step 3: Ice temperature increases
+        change_ice_temp = thermal_conductivity_of_rock * (avg_temp_gradient[-1] - ice_temp[-1])/(density_ice * heatcap_ice)
+        ice_temp.append(ice_temp[-1] - change_ice_temp)
         
-    for i in range(len(surface_temp_gradient)):
-        avg_temp_gradient.append((surface_temp_gradient[i] + bottom_temp_gradient[i])/2)
-    
-    avg_temp.append(np.average(avg_temp_gradient))
-    
-# =============================================================================
-#         if avg_temp_gradient[i] - surface_temp_gradient[i] != 0:
-#             print("Valid")
-# =============================================================================
-    
-    # =============================================================================
-    # plt.plot(surface_temp_gradient, debris_depth)
-    # plt.plot(bottom_temp_gradient, debris_depth)
-    # plt.plot(avg_temp_gradient, debris_depth)
-    # =============================================================================
-    
-    #Step 3: Ice temperature increases
-    change_ice_temp = thermal_conductivity_of_rock * (avg_temp[-1] - ice_temp[-1])/(density_ice * heatcap_ice)
-    ice_temp.append(ice_temp[-1] + change_ice_temp)
-    
+        #update time
+        time += 1
+
     
 #Once ice heats up to melting temperature    
 if ice_temp[-1] > 273:
@@ -90,9 +100,8 @@ if ice_temp[-1] > 273:
 # =============================================================================
         
         #Step 3: Ice mass loss calculation
-        ice_mass_loss = - thermal_conductivity_of_rock * (avg_temp[-1] -ice_temp[-1])/latent_fusion
-        mass_loss.append(ice_mass_loss)
-        ice_mass.append(ice_mass[-1] + ice_mass_loss)
+        ice_mass_loss = - thermal_conductivity_of_rock * (avg_temp_gradient[-1] -ice_temp[-1])/latent_fusion
+        ice_mass.append(ice_mass_loss)
         
         ice_temp.append(ice_temp[-1])
 
@@ -107,8 +116,5 @@ axs[0,1].set_title("Change in ice temperature until melting point")
 
 axs[1,0].plot(range(len(avg_temp)), avg_temp)
 axs[1,0].set_title("Change in average debris layer temperature per iteration")
-
-axs[1,1].plot(range(len(mass_loss)),mass_loss)
-axs[1,1].set_title("Loss in ice mass per iteration")
 
 #axs[1,1].plot(range(len(surface_temp_gradient)), surface_temp_gradient)
